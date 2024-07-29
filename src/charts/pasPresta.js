ffd7ce89964c6416803cace382e5f8549a68d7d9
Raurenpro <raurenpro@gmail.com>
2024-07-29 10:28:58 +0200

    Add title + color + multiple grid + PAS_Presta KPI

---
+++ src/charts/pasPresta.js
@@ -0,0 +1,64 @@
+export const pasPresta = {
+    type: 'pie',
+    dataKey: 'pasPresta',
+    data: {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: [
+            'rgb(34, 197, 94)',
+            'rgb(255, 99, 132)',
+          ],
+          hoverOffset: 4,
+          borderColor: 'rgb(39, 39, 42)',
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      plugins: {
+        title: {
+          align: 'start',
+          display: true,
+          text: 'Nombre de vulnérabilités',
+          color: 'white',
+          font: {
+            size: 24,
+            weight: 'bold',
+          },
+          padding: {
+            bottom: 25,
+          },
+        },
+        legend: {
+          position: 'right',
+          labels: {
+            color: 'white',
+            font: {
+              size: 17,
+            },
+            padding: 20,
+          },
+        },
+        datalabels: {
+          color: 'white',
+          font: {
+            size: 18,
+            weight: 'bold',
+          },
+          formatter: (value, context) => {
+            let totalValues = 0;
+
+            for (let index in context.dataset.data) {
+                totalValues += context.dataset.data[index]
+            }
+
+            let percentage = (value * 100 / totalValues).toFixed(0);
+            return percentage + ' %'; // Affiche la valeur
+          },
+        },
+      },
+    },
+  };
+  
